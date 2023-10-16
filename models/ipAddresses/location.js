c6f28d1e9a39bce36be30d3cc982e86b36883ab9
AhmadTash <144666528+AhmadTash@users.noreply.github.com>
2023-10-16 20:03:39 +0300

    Update location.js

--- models/ipAddresses/location.js
+++ models/ipAddresses/location.js
@@ -12,17 +12,19 @@ const locationSchema = new mongoose.Schema({
     ip: String,
     country: String,
     region: String,
+    type: String,
     time: String
 });
 
 const Location = mongoose.model("Location", locationSchema);
 
 
-async function saving(ip, country, region){
+async function saving(ip, country, region, type){
 const newlocation = new Location({
     ip: ip,
     country: country,
     region: region,
+    type: type,
     time: Date()
 })
 
@@ -36,8 +38,9 @@ await newlocation.save()
 
 const getLocation = async(ip) => {
     const location = await axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_BH68ACXzteSjksNqGt0OsdVUNtSx1&ipAddress=${ip}`)
+    const checkVPN = await axios.get(`https://proxycheck.io/v2/${ip}?&risk=1&vpn=1`)
     const data = location.data
-    saving(data.ip, data.location.country, data.location.region)
+    saving(data.ip, data.location.country, data.location.region, checkVPN.data[ip].type)
 }
 
 
